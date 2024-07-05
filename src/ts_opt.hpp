e4da8412c373decf88ab24fc259d21a3139e6e1c
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-07-06 00:48:41 +0300

    ts_opt fixes

--- src/ts_opt.hpp
+++ src/ts_opt.hpp
@@ -6,19 +6,28 @@
 namespace ansipp {
 
 template <typename T>
-struct ts_opt {
+class ts_opt {
     std::atomic<T> value = {};
     std::atomic_bool is_set = false;
 
     static_assert(std::atomic<T>::is_always_lock_free, "atomic type is not lock-free");
 
+    inline std::logic_error is_set_condition_failed() {
+        return std::logic_error(
+            "double initialization will cause very odd bugs, "
+            "call restore() before calling init()");
+    }
+
+public:
+
     void store(const T& v) {
-        if ( is_set.exchange(true) ) {
-            throw std::logic_error(
-                "double initialization will cause very odd bugs, "
-                "call restore() before calling init()");
+        if ( is_set.load() ) {
+            throw is_set_condition_failed();
         }
         value.store(v);
+        if ( is_set.exchange(true) == true ) {
+            throw is_set_condition_failed();
+        }
     }
 
     template <std::regular_invocable<T> Callback>
