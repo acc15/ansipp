d10e4e53a9d582bc22d4978cda2a1d3580f2559c
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2024-07-23 18:25:33 +0300

    operator>> moved to charbuf

--- include/ansipp/charbuf.hpp
+++ include/ansipp/charbuf.hpp
@@ -30,15 +30,7 @@ class charbuf {
 
 public:
     charbuf(): b(nullptr), e(nullptr), p(nullptr) {}
-
-    charbuf(std::size_t initial_size) {
-        std::size_t s = initial_size < min_alloc_sz ? min_alloc_sz : std::bit_ceil(initial_size);
-        b = static_cast<char*>(std::malloc(s));
-        if (b == nullptr) throw std::bad_alloc();
-        e = b + s;
-        p = b;
-    }
-
+    charbuf(std::size_t initial_size): charbuf() { resize(initial_size); }
     charbuf(charbuf&& mv): b(mv.b), e(mv.e), p(mv.p) { mv.b = mv.e = mv.p = nullptr; }
     ~charbuf() { std::free(b); }
 
@@ -68,40 +60,31 @@ public:
     std::string_view view() const { return std::string_view(b, p); }
     std::string str() const  { return std::string(b, p); }
     
-    charbuf& put(const void* data, std::size_t size) {
-        std::memcpy(reserve(size), data, size); 
-        return *this;
-    }
-
+    charbuf& put(const void* data, std::size_t size) { std::memcpy(reserve(size), data, size); return *this; }
     charbuf& put(char ch) {
         if (p + 1 > e) [[unlikely]] resize(p - b + 1);
         *p++ = ch;
         return *this;
     }
 
-    charbuf& fill(std::size_t count, char ch) { 
-        std::memset(reserve(count), ch, count);
+    charbuf& fill(std::size_t count, char ch) { std::memset(reserve(count), ch, count); return *this; }
+
+    template <typename T>
+    charbuf& operator<<(const T& v) && { return *this << v; }
+
+    charbuf& operator<<(char c) { return put(c); }
+    charbuf& operator<<(bool v) { return put(v ? '1' : '0'); }
+    charbuf& operator<<(const char* sv) { return put(sv, strlen(sv)); }
+    charbuf& operator<<(std::string_view sv) { return put(sv.data(), sv.size()); }
+
+    template <std::integral T>
+    charbuf& operator<<(T v) { 
+        constexpr unsigned int base = 10;
+        unsigned int len = integral_length(v, base);
+        integral_chars(reserve(len), len, v, base, false);
         return *this;
     }
 
 };
-
-inline charbuf& operator<<(charbuf& buf, char c) { return buf.put(c); }
-inline charbuf& operator<<(charbuf& buf, bool v) { return buf.put(v ? '1' : '0'); }
-inline charbuf& operator<<(charbuf& buf, const char* sv) { return buf.put(sv, strlen(sv)); }
-inline charbuf& operator<<(charbuf& buf, std::string_view sv) { return buf.put(sv.data(), sv.size()); }
-
-template <std::integral T>
-inline charbuf& operator<<(charbuf& buf, const T& v) { 
-    constexpr unsigned int base = 10;
-    unsigned int len = integral_length(v, base);
-    integral_chars(buf.reserve(len), len, v, base, false);
-    return buf;
-}
-
-template <typename T>
-inline charbuf& operator<<(charbuf&& buf, const T& v) { 
-    return buf << v;
-}
 
 }
